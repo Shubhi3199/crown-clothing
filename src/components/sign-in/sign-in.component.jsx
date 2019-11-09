1e82ef430f8c70464c73573d113e049848cc8e40
Shubhi3199 <44928935+Shubhi3199@users.noreply.github.com>
2019-11-09 22:18:24 +0530

    Reusable button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './sign-in.styles.scss';
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component{
     constructor(props){
@@ -43,7 +44,7 @@ class SignIn extends React.Component{
                         label={`Password`}
                         required />
 
-                    <input type={`submit`} value={`Submit Form`}/>
+                    <CustomButton type={`submit`}>SIGN IN</CustomButton>
                 </form>
             </div>
         );
